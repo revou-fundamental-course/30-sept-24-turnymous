57ff80ace8c8f9b32a3bdd89ca7f9cf4021eaef2
turnymus <fathurizki125@gmail.com>
2024-10-10 23:38:29 +0700

    Final

--- js/script.js
+++ js/script.js
@@ -1,30 +1,31 @@
-// Function submit
+// Ini javascript
+
+// Function to convert Celsius to Fahrenheit
 function validateForm() {
   const celsiusInput = document.getElementById("input-suhu").value;
   const fahrenheitOutput = document.getElementById("hasil-suhu");
   const kalkulasiOutput = document.getElementById("kalkulasi-suhu");
 
-  // Validasi input
+  // Input validation
   if (celsiusInput === "") {
     alert("Anda belum input nilai!");
     return;
   }
 
   // Convert Celsius to Fahrenheit
+  const celsius = parseFloat(celsiusInput);
   const fahrenheit = (celsius * 9) / 5 + 32;
-  fahrenheitOutput.value = fahrenheit.toFixed(2); // Output formatted to 2 decimal places
 
-  // Output Cara Kalkulasi
+  // Set output values
+  fahrenheitOutput.value = fahrenheit.toFixed(2); // Output formatted to 2 decimal places
   kalkulasiOutput.value = `${celsius} °C x 9/5 + 32 = ${fahrenheit.toFixed(
     2
   )} °F`;
 }
 
-// Function reset
+// Function to reset input and output fields
 function reset() {
   document.getElementById("input-suhu").value = "";
   document.getElementById("hasil-suhu").value = "";
   document.getElementById("kalkulasi-suhu").value = "";
 }
-
-// Function reverse
